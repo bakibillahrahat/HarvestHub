bfe0a356252ee73e3fec2a5b95b7297d1386e94c
bakibillahrahat <bakibillahrahat@protonmail.com>
2023-11-11 00:47:27 +0600

    fix error

---
+++ backend/src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,28 @@
+/* eslint-disable prettier/prettier */
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UserEntity } from 'src/user/user.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(
+    config: ConfigService,
+    @InjectRepository(UserEntity) private userRepo: Repository<UserEntity>,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: config.get('JWT_SECRET'),
+    });
+  }
+  async validate(payload: { sub: number; email: string }) {
+    const user = await this.userRepo.findOneBy({
+      id: payload.sub,
+    });
+    delete user.password
+    return user;
+  }
+}
---
+++ backend/src/user/user.controller.ts
@@ -0,0 +1,27 @@
+import { Body, Controller, Delete, Get, Param, Patch, Put, UseGuards } from '@nestjs/common';
+import { JwtGuard } from 'src/auth/guard';
+import { EditUserDto } from './dto';
+import { UserService } from './user.service';
+import { UserEntity } from './user.entity';
+@UseGuards(JwtGuard)
+@Controller('user')
+export class UserController {
+  constructor(private userService: UserService) {}
+  @Get('viewusers')
+  allUser(): Promise<UserEntity[]> {
+    return this.userService.allUser();
+  }
+  @Get('/:id')
+  searchUserBy(@Param('id') userID: number): Promise<UserEntity> {
+    let user = this.userService.getUserByID(userID);
+    return user;
+  }
+  @Put('edituser/:id')
+  editUser(@Param('id') id: number, @Body() dto: EditUserDto) {
+    return this.userService.editUser(id, dto);
+  }
+  @Delete(':id')
+  deleteUser(@Param('id') id: number){
+    return this.userService.deleteUser(id);
+  }
+}
