ee6f2ff426aeee8453909d11fc778ef5e5f8c45b
bakibillahrahat <bakibillahrahat@protonmail.com>
2023-11-11 19:58:19 +0600

    add logout features

---
+++ backend/src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { AdminModule } from './admin/admin.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { ProductModule } from './product/product.module';
+
+@Module({
+  imports: [
+    AuthModule,
+    UserModule,
+    AdminModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      port: 5434,
+      username: 'admin',
+      password: 'postgres',
+      database: 'scms',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    ConfigModule.forRoot({ isGlobal: true }),
+    ProductModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
