e385edaa975479965fb4fc0fbf71a5b8d137f878
MD. Bakibillah Rahat <bakibillahrahat@protonmail.com>
2023-12-21 22:37:30 +0600

    react hook add

--- frontend/src/components/auth/SignUp.tsx
+++ frontend/src/components/auth/SignUp.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import Link from "next/link";
 import React, { useState } from "react";
@@ -10,54 +10,57 @@ import { FaUser } from "react-icons/fa6";
 import { IoIosArrowBack } from "react-icons/io";
 
 const SignUp = () => {
-  const [name, setName] = useState("");
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const [phone, setPhone] = useState("");
-  const [address, setAddress] = useState("");
-  const [avater, setAvater] = useState("");
+  const [name, setName] = useState<string>("");
+  const [username, setUsername] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [confirmPassword, setConfirmPassword] = useState<string>("");
+  const [phone, setPhone] = useState<string>("");
+  const [address, setAddress] = useState<string>("");
   const [errMsg, setErrMsg] = useState("");
-  const [formErrors, setFormErrors] = useState({
-    email: "",
-    password: "",
-  });
-  const [success, setSuccess] = useState(false);
+  const [formErrors, setFormErrors] = useState<{ [key: string]: string }>({});
+  const [success, setSuccess] = useState<boolean>(false);
 
-  // const validateForm = (): {
-  //   name?: string;
-  //   username?: string;
-  //   email?: string;
-  //   password?: string;
-  //   confirmpassword?: string;
-  //   phone?: string;
-  //   address?: string;
-  //   avater?: string;
-  // } => {
-  //   const errors: {
-  //     name?: string;
-  //     username?: string;
-  //     email?: string;
-  //     password?: string;
-  //     confirmpassword?: string;
-  //     phone?: string;
-  //     address?: string;
-  //     avater?: string;
-  //   };
-  //   if(!name){
-  //     errors.name = "Name is Required",
-  //   }
-  // };
+  const validateForm = () => {
+    const errors: { [key: string]: string } = {};
+    if (!name) {
+      errors.name = "Name is required";
+    } else if (!username) {
+      errors.username = "Username is required";
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+      errors.email = "Invalid email format";
+    }else if(!phone){
+      errors.phone = "Phone number is required!";
+    }else if(!address){
+      errors.address = "Address is required!";
+    }else if()
+    if (!password) {
+      errors.password = "Password is required";
+    } else if (!/[A-Z]/.test(password)) {
+      errors.password = "At least one Capital Letter";
+    } else if (!/[a-z]/.test(password)) {
+      errors.password = "At least one Small Letter";
+    } else if (!/[0-9]/.test(password)) {
+      errors.password = "At least one Number";
+    }
+    if (!confirmPassword) {
+      errors.confirmpassword = "Confirm Password is required";
+    }
+    if (password !== confirmPassword) {
+      errors.confirmpassword = "Password & confirm-Password not Matched";
+    }
+
+    return errors;
+  };
 
   return (
     <div className="bg-white rounded-2xl shadow-2xl flex w-2/3 max-w-4xl">
       {/* Sign Up Page */}
       <div className="w-2/5 bg-green-500 text-white rounded-tl-2xl rounded-bl-2xl py-36 px-12">
-      <Link href={"/"} className="flex text-white text-lg mb-1 -ml-1 -mt-4">
-            <IoIosArrowBack className="m-1"/>
-            Back
-          </Link>
+        <Link href={"/"} className="flex text-white text-lg mb-1 -ml-1 -mt-4">
+          <IoIosArrowBack className="m-1" />
+          Back
+        </Link>
         <h2 className="text-2xl font-bold mb-2">Already Have Account!</h2>
         <div className="border-2 w-10 text-white inline-block mb-2"></div>
         <p className="mb-10">
